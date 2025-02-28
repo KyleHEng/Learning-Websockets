484fac7677b4f550264b23f76503ad8a540860b0
KyleHEng <kylehussaineng@gmail.com>
2025-02-28 11:39:04 +0000

    "Part 4: using express with socket.io"

--- server/index.js
+++ server/index.js
@@ -1,21 +1,45 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const { Server } = require("socket.io");
+const path = require("path");
 
-const httpServer = createServer();
-const io = new Server(httpServer, {
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+
+const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"],
   },
 });
+
+//connection even
 io.on("connection", (socket) => {
   console.log(`user ${socket.id} connected`);
+  //upon connection- sent to user
+  socket.emit("message", "Welcome to Chat App!");
+  //upon connection - sent to everyone else
+  socket.broadcast.emit("message", `user ${socket.id} connected`);
 
+  //Listening for message event
   socket.on("message", (data) => {
     console.log(data);
 
     io.emit("message", `${socket.id} : ${data}`);
   });
-});
 
-httpServer.listen(3500, () => console.log("listening on port 3500"));
+  //When a user disconnects, send message to others in the chat
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("message", `User: ${socket.id} has disconnected`);
+  });
+
+  //Listen for activity
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
+  });
+});
